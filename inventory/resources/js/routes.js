9a4c5f527c69014fffe32e50730a2dc3a425d366
Aleksandr Makogin <makogin.m.servise@gmail.com>
2021-12-27 13:36:08 +0200

    add route login register forget

--- inventory/resources/js/routes.js
+++ inventory/resources/js/routes.js
@@ -1,8 +1,10 @@
 import login from "./components/auth/login";
 import register from "./components/auth/register";
+import forget from "./components/auth/forget";
 
 
 export const routes = [
     { path: '/', component: login, name: '/' },
-    { path: '/register', component: register, name: '/register' }
+    { path: '/register', component: register, name: '/register' },
+    {path: '/forget', component: forget, name: '/forget'}
 ]
